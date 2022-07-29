733ff878d4c12f3bca5b28114717d4ba5006e8ac
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-29 14:58:04 -0300

    Update prototype-here-doc.c

--- prototype-here-doc.c
+++ prototype-here-doc.c
@@ -7,22 +7,26 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	char *doc_temp;
 	char *limiter;
 	char *buf;
 	char *temp_buf;
 	int rd;
+	int doc_size;
 
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	limiter = ft_strdup(argv[1]);
 	rd = read(STDIN_FILENO, buf, BUFFER_SIZE);
-	temp_buf = ft_strdup(buf);
+	buf[rd] = '\0';
+	temp_buf = ft_strdup("");
 	while (rd > 0)
 	{
-		buf[rd] = '\0';
-		if (!ft_strncmp(buf, limiter, ft_strlen(limiter)))
+		temp_buf = ft_strjoin(temp_buf, buf);
+		if (ft_strnstr(temp_buf, limiter, ft_strlen(temp_buf)))
             break ;
         rd = read(STDIN_FILENO, buf, BUFFER_SIZE);
+		buf[rd] = '\0';
 	}
-	ft_printf("%s\n", buf);
+	doc_size = ft_find_limiter(temp_buf, limiter, ft_strlen(temp_buf));
+	temp_buf = ft_substr(temp_buf, 0, doc_size);
+	ft_printf("%s\n", temp_buf);
 }
