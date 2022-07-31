65569a447c65f6c3c7a722a08db88e33bbfda764
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-07-31 18:40:05 -0300

    Update prototype.c

--- prototype.c
+++ prototype.c
@@ -10,6 +10,8 @@
 # define RED				"\033[0;31m"
 # define RESET 				"\033[0m"
 
+# define PLACE_HOLDER 		'x'
+
 # define IN 				0
 # define OUT	 			1
 
@@ -44,6 +46,8 @@ void	exec_last_command(t_data *data, int fd_in, char **envp);
 void	command_not_found_message(char *command);
 void	error(char *message);
 
+int change_spaces_to_place_holder(char *comand, int start);
+void change_ocurrences(char **str, char old_char, char new_char);
 
 int main(int argc, char **argv, char **envp)
 {
@@ -118,7 +122,8 @@ void	init_commands(t_data *data, char **argv)
 	while (i < data->nbr_of_commands)
 	{
 		data->command[i] = split_command(argv[arg]);
-		data->command[i][0] = ft_strjoin("/", data->command[i][0]);
+		if(data->command[i][0][0] != '/')
+			data->command[i][0] = ft_strjoin("/", data->command[i][0]);
 		i++;
 		arg++;
 	}
@@ -130,6 +135,11 @@ void	execute_command(t_data *data, char **cmd_and_flags, char **envp)
 	int 	i;
 
 	i = 0;
+	if (ft_count_occurrences(cmd_and_flags[0], '/') > 1)
+	{
+		execve(cmd_and_flags[0], &cmd_and_flags[0], envp);
+		command_not_found_message(cmd_and_flags[0]);
+	}
 	while (data->paths[i])
 	{
 		command = ft_strjoin(data->paths[i++], cmd_and_flags[0]);
@@ -205,16 +215,73 @@ void command_not_found_message(char *command)
 
 char **split_command(char *command)
 {
-	if (ft_strnstr(command, "awk", ft_strlen("awk")))
+	char 	**command_and_flags;
+	char	*temp_cmd;
+	int		i;
+
+	i = 0;
+	if (ft_strnstr(command, "awk", ft_strlen("awk")) 
+	|| ft_strnstr(command, "sed", ft_strlen("sed")))
 	{
-		error("Working on progress...");
-		return (NULL);
+		temp_cmd = ft_strdup(command);
+		while (temp_cmd[i] != '\0')
+		{
+			if(temp_cmd[i] == '\'' || temp_cmd[i] == '{')
+			{
+				ft_printf("Antes da change space = [%d] = [%c]", i, temp_cmd);
+				i += change_spaces_to_place_holder(command, i);
+				ft_printf("Depois da change space = [%d] = [%d]", i, temp_cmd);
+			}
+			//ft_printf("[%d] -  [%d]\n", temp_cmd[i], i);
+			i++;
+		}
 	}
 	else
 		return (ft_split(command, ' '));
+	i = 0;
+	command_and_flags = ft_split(temp_cmd, ' ');
+	while (command_and_flags[i])
+	{
+		change_ocurrences(&command_and_flags[i], PLACE_HOLDER, ' ');
+		ft_printf("%s\n", command_and_flags[i]);
+	}
+	return (command_and_flags);
 }
 
 void read_here_doc(t_data *data, char *here_doc, char *limiter, char **envp)
 {
 	error("Working on progress...");
+}
+
+int change_spaces_to_place_holder(char *command, int start)
+{
+	int		i;
+	int 	quotes;
+
+	i = start - 1;
+	quotes = 0;
+	while(command[i] != '}' || (command[i] != '\'' && quotes < 2))
+	{
+		if (command[i] == ' ')
+			command[i] = PLACE_HOLDER;
+		if (command[i] != '\'')
+			quotes++;
+		i++;
+		if (command[i] == '\0')
+			return (i);
+	}
+	return (i - 1);
+}
+
+void change_ocurrences(char **str, char old_char, char new_char)
+{
+	int i;
+
+	i = 0;
+	while (*str[i + 1])
+	{
+		if (*str[i] == old_char)
+			*str[i] = new_char;
+		i++;
+	}
 }
