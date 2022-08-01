3fb387ae10dabb5db14469c9c276dd12c3095083
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-01 11:17:39 -0300

    Update pipex.c

--- sources/pipex.c
+++ sources/pipex.c
@@ -5,38 +5,43 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/30 17:30:02 by acesar-l          #+#    #+#             */
-/*   Updated: 2022/07/30 17:31:59 by acesar-l         ###   ########.fr       */
+/*   Created: 2022/08/01 16:14:01 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/08/01 16:16:59 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libraries/pipex.h"
 
-void	execute_command(t_data *data, char **cmd_and_flags, char **envp);
-int		execute_first_command(t_data *data, char **envp);
-void	execute_middle_command(t_data *data, int fd_in, char **envp);
-void	execute_last_command(t_data *data, int fd_in, char **envp);
+void	execute(t_data *data, char **cmd_and_flags, char **envp);
+int		fork_and_exec_first_cmd(t_data *data, char **envp);
+void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp);
+void	exec_last_command(t_data *data, int fd_in, char **envp);
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	t_data *data;
+	t_data	*data;
 	int		fd_pipe_input;
 
 	data = malloc(sizeof(t_data));
 	initialize(data, argc, argv, envp);
-	fd_pipe_input = execute_first_command(data, envp);
+	fd_pipe_input = fork_and_exec_first_cmd(data, envp);
 	if (data->nbr_of_commands == 2)
-			execute_last_command(data, fd_pipe_input, envp);
-	execute_middle_command(data, fd_pipe_input, envp);
+		exec_last_command(data, fd_pipe_input, envp);
+	fork_and_exec_middle_cmd(data, fd_pipe_input, envp);
 	return (0);
 }
 
-void	execute_command(t_data *data, char **cmd_and_flags, char **envp)
+void	execute(t_data *data, char **cmd_and_flags, char **envp)
 {
 	char	*command;
-	int 	i;
+	int		i;
 
 	i = 0;
+	if (ft_count_occurrences(cmd_and_flags[0], '/') > 1)
+	{
+		execve(cmd_and_flags[0], &cmd_and_flags[0], envp);
+		command_not_found_message(cmd_and_flags[0]);
+	}
 	while (data->paths[i])
 	{
 		command = ft_strjoin(data->paths[i++], cmd_and_flags[0]);
@@ -45,10 +50,10 @@ void	execute_command(t_data *data, char **cmd_and_flags, char **envp)
 	command_not_found_message(&cmd_and_flags[0][1]);
 }
 
-int	execute_first_command(t_data *data, char **envp)
+int	fork_and_exec_first_cmd(t_data *data, char **envp)
 {
-	int		fd_pipe[2];
-	int 	pid;
+	int	fd_pipe[2];
+	int	pid;
 
 	pipe(fd_pipe);
 	pid = fork();
@@ -58,20 +63,24 @@ int	execute_first_command(t_data *data, char **envp)
 		close(fd_pipe[IN]);
 		dup2(data->fd_infile, STDIN_FILENO);
 		dup2(fd_pipe[OUT], STDOUT_FILENO);
-		execute_command(data, data->command[0], envp);
+		execute(data, data->command[0], envp);
+		return (0);
 	}
 	else
 	{
 		waitpid(pid, NULL, 0);
 		close(fd_pipe[1]);
+		close(data->fd_infile);
+		if (data->here_doc == true)
+			unlink(data->doc_file);
 		return (fd_pipe[IN]);
 	}
 }
 
-void	execute_middle_command(t_data *data, int fd_in, char **envp)
+void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp)
 {
-	int fd_new_pipe[2];
-	int pid;
+	int	fd_new_pipe[2];
+	int	pid;
 
 	data->atual_command++;
 	pipe(fd_new_pipe);
@@ -81,7 +90,7 @@ void	execute_middle_command(t_data *data, int fd_in, char **envp)
 		close(fd_new_pipe[IN]);
 		dup2(fd_in, STDIN_FILENO);
 		dup2(fd_new_pipe[OUT], STDOUT_FILENO);
-		execute_command(data, data->command[data->atual_command], envp);
+		execute(data, data->command[data->atual_command], envp);
 	}
 	else
 	{
@@ -89,15 +98,15 @@ void	execute_middle_command(t_data *data, int fd_in, char **envp)
 		close(fd_in);
 		close(fd_new_pipe[OUT]);
 		if (data->atual_command == data->nbr_of_commands - 2)
-			execute_last_command(data, fd_new_pipe[0], envp);
-		execute_middle_command(data, fd_new_pipe[0], envp);
+			exec_last_command(data, fd_new_pipe[0], envp);
+		fork_and_exec_middle_cmd(data, fd_new_pipe[0], envp);
 	}
 }
 
-void	execute_last_command(t_data *data, int fd_in, char **envp)
+void	exec_last_command(t_data *data, int fd_in, char **envp)
 {
 	dup2(fd_in, STDIN_FILENO);
 	dup2(data->fd_outfile, STDOUT_FILENO);
-	execute_command(data, data->command[data->nbr_of_commands - 1], envp);
+	execute(data, data->command[data->nbr_of_commands - 1], envp);
 	exit (127);
 }
