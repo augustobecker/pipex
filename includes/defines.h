8e4acd47e43072ac219742bd0986e1243d3e0600
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-08 14:09:17 -0300

    Create defines.h

---
+++ includes/defines.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 19:07:56 by acesar-l          #+#    #+#             */
+/*   Updated: 2022/08/08 19:08:50 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# define GREY 			"\033[0;90m"
+# define RED			"\033[0;31m"
+# define RESET 			"\033[0m"
+
+# define PLACE_HOLDER 		16
+# define FILE_HEREDOC		"/tmp/here_doc"
+# define FILE_HEREDOC_EXTRA	"/tmp/here_doc_extra"
+# define TEMP_FILE		"/tmp/unused"	
+
+# define IN 			0
+# define OUT	 		1
+
+#endif
