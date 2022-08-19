37e12d5e91b8877f580cd80a1b5825f6f7fadf6d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-19 04:35:50 -0300

    Update prototype.c

--- prototype.c
+++ prototype.c
@@ -21,16 +21,36 @@ void	count_not_used_cmds(t_data *data, int argc, char **argv, char **envp);
 void	execute_not_used_cmds(t_data *data, int command, char **envp);
 void	execute(t_data *data, char **cmd_and_flags, char **envp);
 int		fork_and_exec_first_cmd(t_data *data, char **envp);
-void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp);
+void	fork_and_exec_cmd(t_data *data, int fd_in, char **envp);
 void	exec_last_command(t_data *data, int fd_in, char **envp);
 void	command_not_found_message(char *command);
-void	error(char *message, int exit_code);
+void	error(t_data *data, char *message, int exit_code);
 int		change_spaces_to_place_holder(char **comand, int start);
 void 	change_ocurrences(char **str, char old_c, char new_c);
 void 	treat_spaces_inside_the_command(char **command);
 int 	change_ocurrences_until_limiter(char **str, char old_c, char new_c, char limiter);
 int 	change_ocurrences_til_limiter(char **str, char old_c, char new_c, char limiter, int start);
 
+char	*ft_strappend(const char *s1, char **s2);
+char	*ft_substr_n_free(char **s, unsigned int start, size_t len);
+char	*ft_strtrim_n_free(char **s1, char const *set);
+
+// Colocar na Libft
+void	free_str_array(char **array);
+void	free_commands(t_data *data);
+
+// Checar permissões logo de cara
+
+void free_data_and_exit(t_data *data, int exit_code)
+{
+	if (data->cmds_alloc == true)
+	{
+		free_commands(data);
+		free_str_array(data->paths);
+	}
+	free(data);
+	exit (exit_code);
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -40,9 +60,8 @@ int main(int argc, char **argv, char **envp)
 	data = malloc(sizeof(t_data));
 	initialize(data, argc, argv, envp);
 	fd_pipe_input = fork_and_exec_first_cmd(data, envp);
-	if (data->actual_command == data->nbr_of_commands - 1)
-			exec_last_command(data, fd_pipe_input, envp);
-	fork_and_exec_middle_cmd(data, fd_pipe_input, envp);
+	fork_and_exec_cmd(data, fd_pipe_input, envp);
+	free_data_and_exit(data, 1);
 	return (0);
 }
 
@@ -52,7 +71,7 @@ void initialize(t_data *data, int argc, char **argv, char **envp)
 
 	i = 0;
 	if (argc < 5)
-		error("Some arguments are missing", 1);
+		error(data, "Some arguments are missing", 1);
 	init_data(data, argc);
 	data->here_doc.is_present = is_here_doc_present(data, argc, argv);
 	check_for_null_argv(data, argc, argv);
@@ -63,12 +82,15 @@ void initialize(t_data *data, int argc, char **argv, char **envp)
 	init_files(data, argv[1], argv[argc - 1]);
 	find_paths_to_command(data, envp);
 	init_commands(data, argc, argv);
+	data->cmds_alloc = true;
 	if (data->not_used_cmds)
 	{
 		while (data->actual_command < data->not_used_cmds)
 			execute_not_used_cmds(data, i, envp);
 		unlink(TEMP_FILE);
 	}
+	if (argv[argc - 2][0] == 0)
+		error(data, "pipex: : command not found", 127);
 }
 
 void init_data(t_data *data, int argc)
@@ -76,6 +98,7 @@ void init_data(t_data *data, int argc)
 	data->actual_command = 0;
 	data->nbr_of_commands = argc - 3;
 	data->not_used_cmds = 0;
+	data->cmds_alloc = false;
 	data->null_cmd = false;
 	data->here_doc.is_present = false;
 	data->here_doc.is_necessary = false;
@@ -86,7 +109,7 @@ t_bool is_here_doc_present(t_data *data, int argc, char **argv)
 	if (!ft_strncmp(argv[1], "here_doc", ft_strlen("here_doc")))
 	{
 		if (argc < 6)
-			error("Some arguments are missing", 1);
+			error(data, "Some arguments are missing", 1);
 		data->nbr_of_commands = argc - 3;
 		return (true);
 	}
@@ -100,10 +123,10 @@ void	check_for_null_argv(t_data *data, int argc, char **argv)
 
 	i = 2;
 	if (argv[argc - 1][0] == 0)
-		error("pipex: : No such file or directory\n", 1);
+		error(data, "pipex: : No such file or directory", 1);
 	if (data->here_doc.is_present == true)
 		i++;
-	while (i < (argc - 2))
+	while (i < (argc - 1))
 	{
 		if (argv[i][0] == 0)
 		{
@@ -115,8 +138,6 @@ void	check_for_null_argv(t_data *data, int argc, char **argv)
 		}
 		i++;
 	}
-	if (argv[argc - 2][0] == 0)
-		error("pipex: : command not found\n", 127);
 }
 
 void init_here_doc(t_data *data, char *limiter, char *outfile, char **envp)
@@ -126,10 +147,10 @@ void init_here_doc(t_data *data, char *limiter, char *outfile, char **envp)
 
 	data->fd_infile = open(HEREDOC_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (data->fd_infile == -1)
-		error(GREY"pipex : couldn't read here_doc\n"RESET, 1);
+		error(data, "pipex : couldn't read here_doc", 1);
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-		error(GREY"pipex : couldn't alloc here_doc\n"RESET, 1);
+		error(data, "pipex : couldn't alloc here_doc", 1);
 	dup2(data->fd_infile, STDOUT_FILENO);
 	rd = 1;
 	while (rd > 0)
@@ -176,28 +197,25 @@ void init_files(t_data *data, char *infile, char *outfile)
 			close(data->fd_infile);
 			data->fd_infile = open(HEREDOC_FILE, O_RDONLY);
 			if (data->fd_infile == -1)
-				error("infile: something unexpected happened", 1);
+				error(data, "infile: something unexpected happened", 1);
 		}
 		data->fd_outfile = open(outfile, O_WRONLY | O_CREAT | O_APPEND, 0777);
 		if (data->fd_outfile == -1)
-			error("outfile: something unexpected happened", 1);
+			error(data, "outfile: something unexpected happened", 1);
 	}
 	else
 	{
 		data->fd_outfile = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (data->fd_outfile == -1)
-			error("No such file or directory", 1);
+			error(data, "No such file or directory", 1);
 		data->fd_infile = open(infile, O_RDONLY);
 		if (data->fd_infile == -1)
-		{
-			ft_printf(GREY"pipex: %s: No such file or directory\n"RESET, infile);
-			error("", 1);
-		}
+			ft_printf(GREY"pipex: %s: \
+No such file or directory\n"RESET, infile);
 		if (data->not_used_cmds)
 		{
 			data->fd_temp_file = open(TEMP_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (data->fd_temp_file == -1)
-				ft_printf(GREY"pipex: %s: No such file or directory\n"RESET, infile);
+			data->fd_infile = open(TEMP_FILE, O_RDONLY | O_CREAT | O_TRUNC, 0644);
 		}
 	}
 }
@@ -210,7 +228,7 @@ void find_paths_to_command(t_data *data, char **envp)
 	while (!ft_strnstr(envp[i], "PATH=", ft_strlen("PATH=")))
 		i++;
 	data->paths = ft_split(envp[i] + ft_strlen("PATH="), ':');
-	data->paths[0] = ft_substr(data->paths[0], ft_strlen("PATH="), ft_strlen(data->paths[0]));
+	data->paths[0] = ft_substr_n_free(&data->paths[0], ft_strlen("PATH="), ft_strlen(data->paths[0]));
 }
 
 void	init_commands(t_data *data, int argc, char **argv)
@@ -224,18 +242,39 @@ void	init_commands(t_data *data, int argc, char **argv)
 		arg++;
 	data->command = malloc(sizeof(char ***) * (data->nbr_of_commands + 1));
 	if (!data->command)
-		error(GREY"pipex: Couldn't alloc the commands\n"RESET, 1);
+		error(data, "pipex: Couldn't alloc the commands", 1);
 	while (arg < argc - 1)
 	{
 		if (argv[arg][0] != 0)
 		{
 			data->command[i] = split_command(argv[arg]);
 			if(data->command[i][0][0] != '/')
-				data->command[i][0] = ft_strjoin("/", data->command[i][0]);
+				data->command[i][0] = ft_strappend("/", &data->command[i][0]);
 			i++;
 		}
 		arg++;
 	}
+}
+
+char **split_command(char *command)
+{
+	char 	**array_command;
+	char 	*full_command;
+	int		i;
+
+	i = 0;
+	full_command = ft_strdup(command);
+	treat_spaces_inside_the_command(&full_command);
+	array_command = ft_split(full_command, ' ');
+	free(full_command);
+	while (array_command[i])
+	{
+		change_ocurrences(&array_command[i], PLACE_HOLDER, ' ');               
+		array_command[i] = ft_strtrim_n_free(&array_command[i], "\'");
+		array_command[i] = ft_strtrim_n_free(&array_command[i], "\"");
+		i++;
+	}
+	return (array_command);
 }
 
 void execute_not_used_cmds(t_data *data, int command, char **envp)
@@ -248,7 +287,6 @@ void execute_not_used_cmds(t_data *data, int command, char **envp)
 		dup2(data->fd_infile, STDIN_FILENO);
 		dup2(data->fd_temp_file, STDOUT_FILENO);
 		execute(data, data->command[data->actual_command], envp);
-		exit(1);
 	}
 	else
 	{
@@ -257,10 +295,10 @@ void execute_not_used_cmds(t_data *data, int command, char **envp)
 	}
 }
 
-void	error(char *message, int exit_code)
+void	error(t_data *data, char *message, int exit_code)
 {
-	ft_printf(GREY"%s"RESET, message);
-	exit (exit_code);
+	ft_printf(GREY"%s\n"RESET, message);
+	free_data_and_exit(data, exit_code);
 }
 
 void	execute(t_data *data, char **cmd_and_flags, char **envp)
@@ -278,9 +316,11 @@ void	execute(t_data *data, char **cmd_and_flags, char **envp)
 	{
 		command = ft_strjoin(data->paths[i++], cmd_and_flags[0]);
 		execve(command, &cmd_and_flags[0], envp);
+		free(command);
 	}
 	command_not_found_message(&cmd_and_flags[0][1]);
-	exit(0);
+	if (data->actual_command != data->nbr_of_commands - 1)
+		free_data_and_exit(data, 0);
 }
 
 int	fork_and_exec_first_cmd(t_data *data, char **envp)
@@ -293,11 +333,10 @@ int	fork_and_exec_first_cmd(t_data *data, char **envp)
 	if (pid == 0)
 	{
 		close(fd_pipe[IN]);
+		if (data->null_cmd == true)
+			free_data_and_exit(data, 1);
 		dup2(data->fd_infile, STDIN_FILENO);
 		dup2(fd_pipe[OUT], STDOUT_FILENO);
-		if (data->null_cmd == true)
-			exit (0);
-		data->actual_command++;
 		execute(data, data->command[data->actual_command], envp);
 		return (0);
 	}
@@ -305,14 +344,15 @@ int	fork_and_exec_first_cmd(t_data *data, char **envp)
 	{
 		waitpid(pid, NULL, 0);
 		close(fd_pipe[1]);
-		close(data->fd_infile);
+		if (data->fd_infile != -1)
+			close(data->fd_infile);
 		if (data->here_doc.is_present == true)
 			unlink(HEREDOC_FILE);
 		return (fd_pipe[IN]);
 	}
 }
 
-void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp)
+void	fork_and_exec_cmd(t_data *data, int fd_in, char **envp)
 {
 	int fd_new_pipe[2];
 	int pid;
@@ -323,8 +363,12 @@ void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp)
 	{
 		close(fd_new_pipe[IN]);
 		dup2(fd_in, STDIN_FILENO);
-		dup2(fd_new_pipe[OUT], STDOUT_FILENO);
+		if (data->actual_command == data->nbr_of_commands - 1)
+			dup2(data->fd_outfile, STDOUT_FILENO);
+		else
+			dup2(fd_new_pipe[OUT], STDOUT_FILENO);
 		execute(data, data->command[data->actual_command], envp);
+		error(data, "", 127);
 	}
 	else
 	{
@@ -332,45 +376,16 @@ void	fork_and_exec_middle_cmd(t_data *data, int fd_in, char **envp)
 		close(fd_in);
 		close(fd_new_pipe[OUT]);
 		data->actual_command++;
-		if (data->actual_command == data->nbr_of_commands - 1)
-			exec_last_command(data, fd_new_pipe[0], envp);
-		fork_and_exec_middle_cmd(data, fd_new_pipe[0], envp);
+		if (data->actual_command == data->nbr_of_commands)
+			return ;
+		fork_and_exec_cmd(data, fd_new_pipe[0], envp);
 	}
-}
-
-void	exec_last_command(t_data *data, int fd_in, char **envp)
-{
-	dup2(fd_in, STDIN_FILENO);
-	dup2(data->fd_outfile, STDOUT_FILENO);
-	execute(data, data->command[data->nbr_of_commands - 1], envp);
-	exit (127);
 }
 
 void command_not_found_message(char *command)
 {
 	dup2(STDERR_FILENO, STDOUT_FILENO);
 	ft_printf(GREY"pipex: %s : command not found\n"RESET, command);
-	exit (0);
-}
-
-char **split_command(char *command)
-{
-	char 	**array_command;
-	char 	*full_command;
-	int		i;
-
-	i = 0;
-	full_command = ft_strdup(command);
-	treat_spaces_inside_the_command(&full_command);
-	array_command = ft_split(full_command, ' ');
-	while (array_command[i])
-	{
-		change_ocurrences(&array_command[i], PLACE_HOLDER, ' ');               
-		array_command[i] = ft_strtrim(array_command[i], "\'");
-		array_command[i] = ft_strtrim(array_command[i], "\"");
-		i++;
-	}
-	return (array_command);
 }
 
 void treat_spaces_inside_the_command(char **command)
@@ -414,4 +429,105 @@ int change_ocurrences_til_limiter(char **str, char old_c, char new_c, char limit
 		i++;
 	}
 	return (i - start);
+}
+
+void	free_commands(t_data *data)
+{
+	int	cmds;
+
+	cmds = 0;
+	while (cmds < data->nbr_of_commands)
+	{
+		free_str_array(data->command[cmds]);
+		cmds++;
+	}
+	free(data->command);
+}
+
+void	free_str_array(char **array)
+{
+	int	string;
+
+	string = 0;
+	while (array[string])
+		free(array[string++]);
+	free(array);
+}
+
+char	*ft_strappend(const char *s1, char **s2)
+{
+	char	*str;
+
+	if (!s1 || !s2[0])
+		return (NULL);
+	str = (char *)ft_calloc((ft_strlen(s1) + ft_strlen(s2[0])) + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcat(str, s2[0], ft_strlen(s1) + ft_strlen(s2[0]) + 1);
+	free(s2[0]);
+	return (str);
+}
+
+char	*ft_substr_n_free(char **s, unsigned int start, size_t len)
+{
+	size_t	substr_length;
+	char	*substr;
+
+	if (!*s)
+		return (NULL);
+	substr_length = ft_strlen(*s) - start;
+	if (ft_strlen(*s) > start)
+	{
+		if (substr_length > len)
+			substr = (char *) ft_calloc(len + 1, sizeof(char));
+		else
+			substr = (char *) ft_calloc(substr_length + 1, sizeof(char));
+		if (!substr)
+			return (NULL);
+		ft_strlcpy(substr, &s[0][start], len + 1);
+	}
+	else
+		substr = (char *) ft_calloc(1, sizeof(char));
+	free(*s);
+	return (substr);
+}
+
+static int	ft_isunwanted(char c, char const *unwanted)
+{
+	int	i;
+
+	i = 0;
+	while (unwanted[i])
+	{
+		if (unwanted[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim_n_free(char **s1, char const *set)
+{
+	size_t	start;
+	size_t	final;
+	char	*str;
+
+	if (!*s1 || !set)
+		return (NULL);
+	start = 0;
+	final = ft_strlen(*s1) - 1;
+	while (ft_isunwanted(s1[0][start], set) && s1[0][start])
+		start++;
+	while (ft_isunwanted(s1[0][final], set) && final > 0)
+		final--;
+	if (final < start)
+	{
+		str = ft_calloc(1, 1);
+		return (str);
+	}
+	str = ft_substr_n_free(s1, start, final - start + 1);
+	if (!str)
+		return (NULL);
+	return (str);
 }
